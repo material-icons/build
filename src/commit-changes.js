6d727dad70d539169c1586fcc046b6b39f894d84
Vjacheslav Trushkin <cyberalien@gmail.com>
2020-03-25 21:57:49 +0200

    Automatically publish master branch to NPM

--- src/commit-changes.js
+++ src/commit-changes.js
@@ -1,10 +1,36 @@
 'use strict';
 
+const fs = require('fs');
 const config = require('./config');
 const exec = require('./exec');
 
 module.exports = (branch, added, updated, publish) =>
 	new Promise((fulfill, reject) => {
+		let npmPublish = publish && branch === 'master';
+
+		// Bump version
+		if (npmPublish) {
+			const filename = config.outputDir + '/package.json';
+			let data = fs.readFileSync(filename, 'utf8');
+			data = JSON.parse(data);
+
+			let version = data.version.split('.');
+			let patch = version.pop();
+			if (!patch.match(/^[0-9]+$/)) {
+				console.error(
+					'Cannot bump version in package.json - invalid version number.'
+				);
+				npmPublish = false;
+			} else {
+				patch = parseInt(patch) + 1;
+				version.push(patch);
+				data.version = version.join('.');
+				data = JSON.stringify(data, null, 2) + '\n';
+				console.log('Bumped package version to', data.version);
+				fs.writeFileSync(filename, data, 'utf8');
+			}
+		}
+
 		// Commit message
 		let message = '';
 		if (added) {
@@ -26,6 +52,9 @@ module.exports = (branch, added, updated, publish) =>
 		}
 		if (publish) {
 			commands.push('git push origin ' + branch);
+			if (npmPublish) {
+				commands.push('npm publish');
+			}
 		}
 
 		// Commit changes
